8b0ca589fc866b444a5a0f5e6f843a95cb993627
dmitrybv <dimabol@yandex.ru>
2023-08-11 14:50:34 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+      this.myControl1 = new WindowsFormsControlLibrary1.MyControl();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+      this.dataGridView1.Location = new System.Drawing.Point(301, 106);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.Size = new System.Drawing.Size(285, 150);
+      this.dataGridView1.TabIndex = 0;
+      // 
+      // Column1
+      // 
+      this.Column1.HeaderText = "Column1";
+      this.Column1.Name = "Column1";
+      // 
+      // Column2
+      // 
+      this.Column2.HeaderText = "Column2";
+      this.Column2.Name = "Column2";
+      // 
+      // myControl1
+      // 
+      this.myControl1.Location = new System.Drawing.Point(44, 106);
+      this.myControl1.Name = "myControl1";
+      this.myControl1.Size = new System.Drawing.Size(209, 60);
+      this.myControl1.TabIndex = 1;
+      this.myControl1.Text = "myControl1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(685, 390);
+      this.Controls.Add(this.myControl1);
+      this.Controls.Add(this.dataGridView1);
+      this.Name = "Form1";
+      this.Text = "Form1 .NET Framework 4.7.2";
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataGridView dataGridView1;
+    private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    private WindowsFormsControlLibrary1.MyControl myControl1;
+  }
+}
+
